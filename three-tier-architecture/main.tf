67b707f83e9bc8230695b39d85f519751bafa250
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-04-09 21:57:29 +0000

    Drata Compliance Recommendations (Grouped)

--- three-tier-architecture/main.tf
+++ three-tier-architecture/main.tf
@@ -36,7 +36,7 @@ resource "aws_subnet" "web-subnet-2" {
   vpc_id                  = aws_vpc.my-vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "Web-2b"
